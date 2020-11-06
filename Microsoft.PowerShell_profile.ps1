fc46e1dc08f1addcf93aba5685ba64586f8e507b
Cory Koch <CKoch@arcos-inc.com>
2020-11-06 11:38:38 -0500

    add more aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -14,12 +14,31 @@ Set-Alias -Name eprofile -Value editProfile
 function pushprofilehome { Push-Location -Path C:\Users\CKoch\Documents\PowerShell}
 Set-Alias -Name profilehome -Value pushprofilehome
 
-function cdra {Push-Location -Path C:\Users\CKoch\Documents\Source\arcos_rosterapps_new\}
+function cdra {Push-Location -Path C:\Users\CKoch\Documents\Source\arcos_rosterapps_new\RosterApps.Web\}
 Set-Alias -Name ra -Value cdra
 
-function cdram {Push-Location -Path C:\Users\CKoch\Documents\Source\RosterAppsMobile\RosterAppsMobile}
+function cdram {Push-Location -Path C:\Users\CKoch\Documents\Source\RosterAppsMobile\RosterAppsMobile\www}
 Set-Alias -Name ram -Value cdram
+
+# Run mobile app out of www
+function server {http-server -o}
 
 function cdrapi {Push-Location -Path C:\Users\CKoch\Documents\Source\mobile-api}
 Set-Alias -Name rapi -Value cdrapi
 
+function cdrauth {Push-Location -Path C:\Users\CKoch\Documents\Source\RosterApps.Authenticate.API}
+Set-Alias -Name rauth -Value cdrauth
+
+function cdrad {Push-Location -Path C:\Users\CKoch\Documents\Source\arcos_rosterapps_database\}
+Set-Alias -Name rad -Value cdrad
+
+# Git 
+function gitPull { git pull}
+Set-Alias -name gpull -Value gitPull
+
+function gitFetch { git fetch}
+Set-Alias -name gfetch -Value gitFetch
+
+function functions {
+	Write-Host "source, eprofile, profilehome, ra, ram, server, rapi, rad, rauth, gp, gf"
+}
