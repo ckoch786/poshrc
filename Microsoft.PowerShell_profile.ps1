a13be898ba9199cd5a96f0eae4873bd07adaed52
Cory Koch <CKoch@arcos-inc.com>
2021-02-16 12:31:46 -0500

    Add autocompletion for dotnet cli

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -37,3 +37,11 @@ Set-Alias -name gfetch -Value gitFetch
 
 function gitFetch { git status}
 Set-Alias -name gstatus -Value gitFetch
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+     param($commandName, $wordToComplete, $cursorPosition)
+         dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+         }
+ }
